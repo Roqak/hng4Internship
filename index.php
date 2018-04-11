7208ae77f01932931b3cfa1dc68f4058f242aad1
roqak <roqak1@gmail.com>
2018-04-11 07:48:00 -1100

    initial commit
    initial commit

---
+++ index.php
@@ -0,0 +1,59 @@
+
+<html>
+<head>
+	<style type="text/css">
+	header{
+		background-color: #669FD4;
+		width: 100%;
+		margin-top: -20px;
+		height: 10%;
+	}
+	.white-t{
+		color: white;
+	}
+	.loo{
+		color: #D4CDCD;
+		height: 40%;
+	}
+	footer{
+		background-color: #669FD4;
+		height: 46.9%;
+
+	}
+	.pad-down{
+		padding-top: 1.8%;
+	}
+	.con{
+		font-size: 50px;
+	}
+	.ll{
+		padding-top: 60px;
+
+	}
+	table{
+		padding-top: 30px;
+	}
+	</style>
+</head>
+<body>
+	<header>
+		<center>
+		<h1 class="white-t pad-down">
+			HNG
+		</h1>
+		</center>
+	</header>
+	<div class="loo">
+		<center>
+		<h1 class="con ll">HNG</h1><h3 class="con">INTERNSHIP 4</h3>
+		</center>
+	</div>
+	<footer>
+		<table>
+			<tr><td><h1 class="white-t">The Current Time is:</h1></td><td><h2 class="white-t"><?php echo date("h:i:sa"); ?></h2></td></tr>
+			<tr><td><h1 class="white-t">Today's Date is:</h1></td><td><h2 class="white-t"><?php echo date("Y/m/d"); ?></h2></td></tr>
+
+		</footer>
+	</div>
+</body>
+</html>
